91962ae0db3a745204503ea92575abd77856612a
Collin Stedman <cstedman@princeton>
2013-12-20 19:07:32 -0600

    Playing with home view

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 /* Express */
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname + '/public'));
+
+/* Jade */
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
-  res.send('hey');
+  res.render('index');
 });
 
 app.listen(5000);
